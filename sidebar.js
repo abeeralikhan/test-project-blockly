b47c9463d310cce0371ac493abd8cf1d71c4c48b
Hamna Siddiqui <hamnas200@gmail.com>
2023-01-03 22:30:14 +0500

    check these new changes in sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -3,6 +3,9 @@
 // console.log(root.children);
 
 const menu_driven_div = document.querySelector(".parent-overlay-bar");
+const var_div = document.querySelector(".div1");
+const for_div = document.querySelector(".div2");
+const if_div = document.querySelector(".div3");
 const side_menu = document.querySelectorAll(".sides");
 
 // const htmlDOM = document.querySelector(".bodytag");
@@ -15,17 +18,50 @@ let isClicked = true;
 //   }
 // }
 
-side_menu.forEach((e) => {
-  e.addEventListener("click", () => {
-    if (isClicked) {
-      menu_driven_div.style.visibility = "visible";
-      isClicked = false;
-    } else {
-      menu_driven_div.style.visibility = "hidden";
-      isClicked = true;
-    }
-  });
-});
+const makehide = () => {
+  if (var_div.classList.contains("hidden")) {
+    var_div.classList.remove("hidden");
+    var_div.classList.add("visible");
+  } else {
+    var_div.classList.remove("visible");
+    var_div.classList.add("hidden");
+  }
+  // console.log(var_div.classList.contains("visible"));
+};
+
+const makehide2 = () => {
+  if (for_div.classList.contains("hidden")) {
+    for_div.classList.remove("hidden");
+    for_div.classList.add("visible");
+  } else {
+    for_div.classList.remove("visible");
+    for_div.classList.add("hidden");
+  }
+  // console.log(var_div.classList.contains("visible"));
+};
+
+const makehide3 = () => {
+  if (if_div.classList.contains("hidden")) {
+    if_div.classList.remove("hidden");
+    if_div.classList.add("visible");
+  } else {
+    if_div.classList.remove("visible");
+    if_div.classList.add("hidden");
+  }
+  // console.log(var_div.classList.contains("visible"));
+};
+
+// side_menu.forEach((e) => {
+//   e.addEventListener("click", () => {
+//     if (isClicked) {
+//       menu_driven_div.style.visibility = "visible";
+//       isClicked = false;
+//     } else {
+//       menu_driven_div.style.visibility = "hidden";
+//       isClicked = true;
+//     }
+//   });
+// });
 
 // const display_div = () => {
 //   if (isClicked) {
