3038f566fff1b663827008093ad17958efc8b2c0
Abeer Ali Khan <aalkhan@students.uit.edu>
2023-01-13 01:33:07 +0500

    refact function and remove unnecessary variable

--- sidebar.js
+++ sidebar.js
@@ -1,9 +1,8 @@
 const sidebarLis = document.querySelectorAll(".navs > p");
 const subSidebars = document.querySelectorAll(".target-div");
-const navLis = document.querySelectorAll(".navs");
 const closeButtons = document.querySelectorAll(".sidebar-header > span");
 
-function getColorMappings(constructType) {
+function getBlockColorClass(constructType) {
   switch (constructType) {
     case "math-subside":
       return "math-color";
@@ -22,7 +21,7 @@ function getColorMappings(constructType) {
   }
 }
 
-const makeAllHiddenAndRemoveColor = (ignoreSubSidebar = "") => {
+function makeAllHiddenAndRemoveColor(ignoreSubSidebar = "") {
   subSidebars.forEach((subSidebar) => {
     if (
       !subSidebar.classList.contains("hidden") &&
@@ -30,54 +29,12 @@ const makeAllHiddenAndRemoveColor = (ignoreSubSidebar = "") => {
     ) {
       subSidebar.classList.add("hidden");
       subSidebar.parentNode.classList.remove(
-        getColorMappings(subSidebar.classList[1])
+        getBlockColorClass(subSidebar.classList[1])
       );
       subSidebar.parentNode.classList.remove("fg-color");
     }
   });
-};
-
-// const stylings = (
-//   target_div,
-//   target_nav,
-//   inactive_nav1,
-//   inactive_nav2,
-//   target_block,
-//   colorCode
-// ) => {
-//   if (target_div.classList.contains("visible")) {
-//     target_nav.style.backgroundColor = colorCode;
-//     target_nav.style.color = "white";
-//     inactive_nav1.style.backgroundColor = "";
-//     inactive_nav2.style.backgroundColor = "";
-//     target_block.style.backgroundColor = colorCode;
-//   } else {
-//     target_nav.style.backgroundColor = "";
-//     target_nav.style.color = "";
-//   }
-// };
-
-// const makeClear = (inactive_nav1, inactive_nav2) => {
-//   inactive_nav1.style.color = "";
-//   inactive_nav2.style.color = "";
-// };
-
-// navLis.forEach((sidebarLi) => {
-//   sidebarLi.addEventListener("click", () => {
-//     makeAllHidden();
-//     const subSidebar = sidebarLi.parentNode.querySelector(".target-div");
-//     if (
-//       !sidebarLi.classList.contains("colors") &&
-//       subSidebar.classList.contains("hidden")
-//     ) {
-//       sidebarLi.classList.add("colors");
-//       subSidebar.classList.remove("hidden");
-//     } else {
-//       sidebarLi.classList.remove("colors");
-//     }
-//     console.log(sidebarLi);
-//   });
-// });
+}
 
 sidebarLis.forEach((sidebarLi) => {
   sidebarLi.addEventListener("click", () => {
@@ -86,7 +43,7 @@ sidebarLis.forEach((sidebarLi) => {
 
     subSidebar.classList.toggle("hidden");
     subSidebar.parentNode.classList.toggle(
-      getColorMappings(subSidebar.classList[1])
+      getBlockColorClass(subSidebar.classList[1])
     );
     subSidebar.parentNode.classList.toggle("fg-color");
   });
